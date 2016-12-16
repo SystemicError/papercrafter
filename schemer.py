e67aa723a2c56816c5dc283b2978bfbe97f70c88
Tevis Tsai <tevis.tsai@gmail.com>
2016-12-16 10:04:36 -0500

    Added code to select a region and show this region on screen.

--- schemer.py
+++ schemer.py
@@ -55,26 +55,32 @@ def process_inputs(event, schematic, current_line, current_selection):
     if event.type == sdl2.SDL_MOUSEBUTTONDOWN:
         x = int(event.button.x/float(GRID) + .5)
         y = int(event.button.y/float(GRID) + .5)
-        if len(current_line) == 0:
-            current_line.append([x, y])
-        elif len(current_line) == 1:
-            current_line.append([x, y])
-            line = [current_line.pop(), current_line.pop()]
-            if line in schematic["lines"]:
-                schematic["lines"].pop(schematic["lines"].index(line))
-            else:
-                schematic["lines"].append(line)
+        if event.button.button == sdl2.SDL_BUTTON_LEFT and current_selection == []:
+            if len(current_line) == 0:
+                current_line.append([x, y])
+            elif len(current_line) == 1:
+                current_line.append([x, y])
+                line = [current_line.pop(), current_line.pop()]
+                if line in schematic["lines"]:
+                    schematic["lines"].pop(schematic["lines"].index(line))
+                else:
+                    schematic["lines"].append(line)
+        elif event.button.button == sdl2.SDL_BUTTON_RIGHT and current_line == []:
+            if len(current_selection) == 0:
+                current_selection.append([x, y])
+            elif len(current_selection) == 1:
+                current_selection.append([x, y])
+                adjust_selection(current_selection)
+            elif len(current_selection) == 2:
+                current_selection.pop()
+                current_selection.pop()
 
-def dot_product(v0, v1):
-    products = [v0[i]*v1[i] for i in range(len(v0))]
-    return sum(products)
-
-def point_to_vector(image, pt, focal_length):
-    mm_per_px = 3.39/image.h
-    x = (pt[0] - image.w/2.0)*mm_per_px
-    y = (pt[1] - image.h/2.0)*mm_per_px
-    z = focal_length
-    return [x, y, z]
+def adjust_selection(selection):
+    "Makes sure that the first point is the upper left, second is lower right."
+    upper_left = [min([selection[0][0], selection[1][0]]), min([selection[0][1], selection[1][1]])]
+    lower_right = [max([selection[0][0], selection[1][0]]), max([selection[0][1], selection[1][1]])]
+    selection[0] = upper_left
+    selection[1] = lower_right
 
 def draw_frame(window, schematic, current_line, current_selection):
     "Given the preloaded surface 'image', paint as background."
@@ -82,11 +88,13 @@ def draw_frame(window, schematic, current_line, current_selection):
     ctx.clear()
     render_grid(ctx)
     render_lines(ctx, schematic["lines"])
+    if len(current_selection) == 2:
+        highlight_selection(ctx, current_selection)
     ctx.present()
     return
 
 def render_grid(ctx):
-    color = sdl2.ext.Color(0, 127, 127)
+    color = sdl2.ext.Color(0, 100, 100)
     for i in range(0, GRID_HEIGHT):
         ctx.draw_line([0, i*GRID, PX_WIDTH, i*GRID], color)
     for i in range(0, GRID_WIDTH):
@@ -96,6 +104,16 @@ def render_lines(ctx, lines):
     color = sdl2.ext.Color(255, 255, 255)
     for line in lines:
         ctx.draw_line([line[0][0]*GRID, line[0][1]*GRID, line[1][0]*GRID, line[1][1]*GRID], color)
+
+def highlight_selection(ctx, current_selection):
+    color = sdl2.ext.Color(255, 0, 0)
+    upper_left = current_selection[0]
+    lower_right = current_selection[1]
+    min_x = int((upper_left[0] - .5)*GRID)
+    min_y = int((upper_left[1] - .5)*GRID)
+    max_x = int((lower_right[0] + .5)*GRID)
+    max_y = int((lower_right[1] + .5)*GRID)
+    ctx.draw_line([min_x, min_y, min_x, max_y, max_x, max_y, max_x, min_y, min_x, min_y], color)
 
 if __name__ == "__main__":
     sys.exit(run())
